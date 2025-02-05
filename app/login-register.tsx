b4db959d821931b84214fb6cb78146df4df1be54
Pharnem <bartosz.kaczmarek1714@gmail.com>
2025-02-05 19:03:14 +0100

    Login Register endpoints integration

--- app/login-register.tsx
+++ app/login-register.tsx
@@ -10,6 +10,7 @@ import {
 	Platform,
 	Keyboard,
 	TouchableWithoutFeedback,
+	Alert,
 } from "react-native";
 import SegmentedControl from "@react-native-segmented-control/segmented-control";
 import { useRouter } from "expo-router";
@@ -17,92 +18,51 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginRegisterScreen() {
 	const [activeTab, setActiveTab] = useState(false);
+	const [email, setEmail] = useState("");
 	const router = useRouter();
+
+	const handleAuth = async () => {
+		const endpoint = process.env.EXPO_PUBLIC_API_URL + (activeTab ?  "/customers/login" : "/customers/");
+		try {
+			const response = await fetch(endpoint, {
+				method: activeTab ? "PUT" : "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ email }),
+			});
+
+			if (response.status === 200 || response.status === 201) {
+				router.push("/dashboard");
+			} else if (response.status === 409) {
+				Alert.alert("Error", "Email already exists or invalid credentials.");
+			} else {
+				Alert.alert("Error", "An unexpected error occurred. Please try again.");
+			}
+		} catch (error) {
+			Alert.alert("Error", "Failed to connect to the server.");
+		}
+	};
 
 	return (
 		<SafeAreaView style={styles.container}>
 			{/* Logo */}
 			<View style={styles.logoContainer}>
-				<Image
-					source={require("@/assets/images/logo.png")}
-					style={styles.logo}
-					resizeMode="contain"
-				/>
+				<Image source={require("@/assets/images/logo.png")} style={styles.logo} resizeMode="contain" />
 			</View>
 			<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-				<KeyboardAvoidingView
-					behavior={Platform.OS === "ios" ? "padding" : "height"}
-					style={styles.keyboardAvoidingView}
-				>
+				<KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.keyboardAvoidingView}>
 					{/* Tabs */}
-					<SegmentedControl
-						values={["Register", "Login"]}
-						selectedIndex={activeTab ? 1 : 0}
-						appearance="light"
-						onChange={() => setActiveTab(!activeTab)}
-						style={{ marginBottom: 16 }}
-					/>
+					<SegmentedControl values={["Register", "Login"]} selectedIndex={activeTab ? 1 : 0} appearance="light" onChange={() => setActiveTab(!activeTab)} style={{ marginBottom: 16 }} />
 					{/* Tab Content */}
-					{activeTab ? (
-						<View
-							style={{
-								paddingBottom: 59,
-							}}
-						>
-							<Text style={styles.header}>Login</Text>
-							<Text style={{ color: "#888", marginBottom: 16 }}>
-								Enter your credentials that you used to
-								register.
-							</Text>
-							<TextInput
-								placeholder="Username or Email"
-								style={styles.input}
-								placeholderTextColor={"#888"}
-							/>
-							<TextInput
-								placeholder="Password"
-								secureTextEntry
-								style={styles.input}
-								placeholderTextColor={"#888"}
-							/>
-							<Pressable
-								style={styles.button}
-								onPress={() => router.push("/dashboard")}
-							>
-								<Text style={styles.buttonText}>Login</Text>
-							</Pressable>
-						</View>
-					) : (
-						<View>
-							<Text style={styles.header}>Register</Text>
-							<Text style={{ color: "#888", marginBottom: 16 }}>
-								Enter your email address and choose your
-								username.
-							</Text>
-							<TextInput
-								placeholder="Email"
-								style={styles.input}
-								placeholderTextColor={"#888"}
-							/>
-							<TextInput
-								placeholder="Username"
-								style={styles.input}
-								placeholderTextColor={"#888"}
-							/>
-							<TextInput
-								placeholder="Password"
-								secureTextEntry
-								style={styles.input}
-								placeholderTextColor={"#888"}
-							/>
-							<Pressable
-								style={styles.button}
-								onPress={() => router.push("/dashboard")}
-							>
-								<Text style={styles.buttonText}>Register</Text>
-							</Pressable>
-						</View>
-					)}
+					<View>
+						<Text style={styles.header}>{activeTab ? "Login" : "Register"}</Text>
+						<Text style={{ color: "#888", marginBottom: 16 }}>{activeTab ? "Enter your credentials that you used to register." : "Enter your email address."}</Text>
+						<TextInput placeholder="Email" style={styles.input} placeholderTextColor={"#888"} value={email} onChangeText={setEmail} />
+						<Pressable style={styles.button} onPress={handleAuth}>
+							<Text style={styles.buttonText}>{activeTab ? "Login" : "Register"}</Text>
+						</Pressable>
+					</View>
 				</KeyboardAvoidingView>
 			</TouchableWithoutFeedback>
 		</SafeAreaView>
